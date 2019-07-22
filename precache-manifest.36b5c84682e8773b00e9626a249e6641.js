8581a7dfc3ff11d31d97954bcab39eeb3adf4552
Yashas <yashasbysani@gmail.com>
2019-07-22 11:25:48 -0400

    Updates

---
+++ precache-manifest.36b5c84682e8773b00e9626a249e6641.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d94143ecbd5a36c17971ff6b53130902",
+    "url": "/bikeRentals/index.html"
+  },
+  {
+    "revision": "dee9be2e15501d4fe4f2",
+    "url": "/bikeRentals/static/css/main.9e50b9d0.chunk.css"
+  },
+  {
+    "revision": "894e8c6b94e388495f14",
+    "url": "/bikeRentals/static/js/2.2c79e2fb.chunk.js"
+  },
+  {
+    "revision": "dee9be2e15501d4fe4f2",
+    "url": "/bikeRentals/static/js/main.7a9a02be.chunk.js"
+  },
+  {
+    "revision": "b2fdc5f04ceec7792810",
+    "url": "/bikeRentals/static/js/runtime~main.ec9dd2b4.js"
+  }
+]);
